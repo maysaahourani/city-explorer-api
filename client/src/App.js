b2ddcc8d27fe7275314d8c41df61b10a690d8c07
maysaahourani <maysaahourani@gmail.com>
2021-06-29 03:03:39 +0300

    intialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react'
-// import Forecast from '/Forecast'
-import axios from 'axios'
- class App extends Component {
+import logo from './logo.svg';
+import './App.css';
 
-  getData= async ()=> {
-    let axiosREad =await axios.get('http://localhost:8000/')
-    console.log(axiosREad)
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.getData}>submit</button>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
